cf7c326ffb47c8f27b219b0a2d0a9063471aead0
Patit Pawan Karmakar <patitpawank@gmail.com>
2018-03-21 22:48:55 +0530

    User setters and getters

--- webservice/src/main/java/com/webservice/controller/UserController.java
+++ webservice/src/main/java/com/webservice/controller/UserController.java
@@ -3,8 +3,11 @@ package com.webservice.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.webservice.domain.User;
@@ -17,8 +20,10 @@ public class UserController {
 	UserService userService;
 	
 	@GetMapping("/users")
-	public List<User> getAllUsers() {
-		return userService.getAllUsers();
+	public ResponseEntity<List<User>> getAllUsers() {
+		List<User> users = userService.getAllUsers();
+		System.out.println(users);
+		return new ResponseEntity<List<User>>(users, HttpStatus.OK);
 	}
 	
 }
